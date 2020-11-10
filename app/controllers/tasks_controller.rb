db3de7b0b4facdcf732b8a4256371db83263880c
Zach Ross <zachariahrossy@gmail.com>
2020-11-10 15:52:38 +0100

    last update for task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    task_params = params.require(:task).permit(:name, :details)
+    task_params = params.require(:task).permit(:name, :details, :completed)
     @task = Task.new(task_params)
     @task.save!
     redirect_to task_path(@task)
@@ -21,11 +21,18 @@ class TasksController < ApplicationController
 
   def update
     task_params = params.require(:task).permit(:name, :details, :completed)
+    @task = Task.find(params[:id])
     @task.update!(task_params)
     redirect_to task_path(@task)
   end
 
   def edit
      @task = Task.find(params[:id])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy!
+    redirect_to tasks_path
   end
 end
